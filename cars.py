b296e864619dafe9e8e877d93cdf30b7cb0c84cc
Lonealpha7 <mohdtasnim9@gmail.com>
2024-01-31 11:11:10 +0300

    Add function to return random model year

--- cars.py
+++ cars.py
@@ -1,8 +1,14 @@
+# cars.py
+
 import random
 
 car_brands = ["Toyota", "Ford", "Chevrolet", "Honda", "BMW", "Mercedes-Benz"]
 
+def get_random_model_year():
+    return random.randint(1990, 2024)  
+
 selected_brand = random.choice(car_brands)
 
 print(f"Randomly selected car brand: {selected_brand}")
+print(f"Random model year: {get_random_model_year()}")
 

9cee5633a9ddcb5bab5408b0eec5ea7737f4bab0
Lonealpha7 <mohdtasnim9@gmail.com>
2024-01-31 10:58:47 +0300

    Add feature to randomly select a car brand

--- cars.py
+++ cars.py
@@ -1,1 +1,8 @@
-print("Hello Cars") 
+import random
+
+car_brands = ["Toyota", "Ford", "Chevrolet", "Honda", "BMW", "Mercedes-Benz"]
+
+selected_brand = random.choice(car_brands)
+
+print(f"Randomly selected car brand: {selected_brand}")
+
